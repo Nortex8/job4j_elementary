99e0abed2112c4ce6984921c54b2717ee7b177b8
Nortex8 <ivanov9y@gmail.com>
2022-05-07 23:08:30 +0300

    6.5. Поиск индекса в диапазоне. [#156318 #297902]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -15,12 +15,12 @@ public class FindLoopTest {
 
     @Test
     public void whenDiapasonHas2Then3() {
-        int[] data = new int[]{5, 2, 10, 2, 122, 6, 12};
-        int el = 122;
+        int[] data = new int[]{5, 2, 10, 2, 4};
+        int el = 2;
         int start = 2;
         int finish = 4;
         int result = FindLoop.indexInRange(data, el, start, finish);
-        int expected = 4;
+        int expected = 3;
         Assert.assertEquals(expected, result);
     }
 
