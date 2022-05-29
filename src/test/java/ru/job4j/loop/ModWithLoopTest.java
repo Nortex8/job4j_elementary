3e63d1d0e6b0555c9c5f10e9e9d1a40a337317e6
Nortex8 <ivanov9y@gmail.com>
2022-05-29 17:37:38 +0300

    6.8. Дефрагментация массива [#225 #299112]

--- src/test/java/ru/job4j/loop/ModWithLoopTest.java
+++ src/test/java/ru/job4j/loop/ModWithLoopTest.java
@@ -8,8 +8,10 @@ public class ModWithLoopTest {
 
     @Test
     public void test() {
-        assertEquals(0, ModWithLoop.mod(29, 5));
-
+        assertEquals(0, ModWithLoop.mod(4, 2));
+        assertEquals(1, ModWithLoop.mod(3, 2));
+        assertEquals(2, ModWithLoop.mod(8, 3));
+        assertEquals(0, ModWithLoop.mod(0, 3));
     }
 
 }
