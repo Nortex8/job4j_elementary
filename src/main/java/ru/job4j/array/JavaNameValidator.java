4fa6127b1e5f733aadb4dcbe61d1d1e5dafefd16
Nortex8 <ivanov9y@gmail.com>
2022-11-20 16:19:40 -0500

    6.9. String - это массив [#504884]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -2,42 +2,25 @@ package ru.job4j.array;
 
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-        for (int i = 0; i < name.length(); i++) {
-            if ((isSpecialSymbol(name.codePointAt(i)) || isUpperLatinLetter(name.codePointAt(i)) || isLowerLatinLetter(name.codePointAt(i)) || Character.isDigit(name.codePointAt(i)))
-                    && !Character.isUpperCase(name.codePointAt(0)) && Character.isLowerCase(name.codePointAt(0)) && !name.isEmpty()) {
+        if (!name.isEmpty() && Character.isLowerCase(name.codePointAt(0))) {
+        for (int i = 1; i < name.length(); i++) {
+            if (isSpecialSymbol(name.codePointAt(i)) || isUpperLatinLetter(name.codePointAt(i)) || isLowerLatinLetter(name.codePointAt(i)) || Character.isDigit(name.codePointAt(i))) {
                 return true;
             }
         }
+      }
         return false;
     }
 
     public static boolean isSpecialSymbol(int code) {
-        for (int i = 0; i < code; i++) {
-            if (code == 36 || code == 95) {
-                return true;
-            }
-            break;
-        }
-            return false;
+            return code == 36 || code == 95;
         }
 
         public static boolean isUpperLatinLetter(int code) {
-            for (int i = 0; i < code; i++) {
-                if (code > 64 && code < 91) {
-                    return true;
-                }
-                break;
-            }
-            return false;
+            return code > 64 && code < 91;
         }
 
         public static boolean isLowerLatinLetter(int code) {
-            for (int i = 0; i < code; i++) {
-                if (code > 96 && code < 123) {
-                    return true;
-                }
-                break;
-            }
-            return false;
+            return code > 96 && code < 123;
         }
 }
